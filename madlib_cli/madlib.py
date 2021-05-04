0c89505852e16b3fe46197abab63b72560cdcd12
NOOR OMARI <nooromari847@gmail.com>
2021-05-04 22:34:27 +0300

    raises error

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,40 +1,15 @@
-# import re
-# print('Welcome to Madlib game, please input the blanks')
-
-# def read_template(file_path):
-#     with open(file_path, "r") as f:
-#         return f.read().strip('\n')
-
-# def parse_template(text):
-#     # text = read_template("assets/dark_and_stormy_night_template.txt")
-#     # s =  [e+'' for e in re.split('{|}',text)]
-#     partsre = re.findall(r'\{.*?\}', text) 
-#     print(partsre)
-#     newtext = text.split(' ')
-#     parts = []
-#     for t in newtext:
-#         if not t.find('{'):
-#            parts.append(t)
-#            text=text.replace(t,"{}")
-#     parts = [ele.replace('{','') for ele in parts]
-#     parts = [ele.replace('}','') for ele in parts]
-#     parts_t = ()
-#     for i in parts:
-#         parts_t += (i,)
-#     actual_stripped=text+'.'
-#     actual_parts=parts_t
-#     return actual_stripped, actual_parts
-
-# parse_template(
-#         "It was a {Adjective} and {Adjective} {Noun}."
-#     )
-
 import re
 print('Welcome to Madlib game, please input the blanks')
 
 def read_template(file_path):
-    with open(file_path, "r") as f:
-        return f.read().strip('\n')
+    try:
+      with open(file_path, "r") as f_path:
+       return f_path.read().strip("\n")
+    except FileNotFoundError:
+      raise FileNotFoundError('A very specific bad thing happened.')
+    except:
+       print("Other error")
+    
 
 def parse_template(text):
     parts=[]
@@ -55,5 +30,24 @@ def parse_template(text):
 def merge(text,parts):
     return text.format(*parts)
 
-print(merge("It was a {} and {} {}.", ("dark", "stormy", "night")))
+def create_file(result):
+    with open("assets/make_me_a_video_game_output_from_user.txt", "w") as f:
+        f.write(result)
 
+
+
+print(merge("It was a {} and {} {}.", ("dark", "stormy", "night")))
+def receive_data():
+    text = read_template("assets/make_me_a_video_game_template.txt")
+    stripped_text, parts_tuple = parse_template(text)
+    user_answers = []
+
+    for i in range(len(parts_tuple)):
+        x = input('enter a {} > '.format(parts_tuple[i]))
+        user_answers.append(x)
+    result = stripped_text.format(*user_answers)
+    print(result)
+    create_file(result)
+
+
+# receive_data()
